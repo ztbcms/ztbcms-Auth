612a8ae9ea9197e50dbb034a31d460212d860bd9
Jayin Ton <tonjayin@gmail.com>
2019-08-24 16:37:16 +0800

    update

--- Controller/TestAuthController.class.php
+++ Controller/TestAuthController.class.php
@@ -6,11 +6,18 @@
 
 namespace Auth\Controller;
 
+use Auth\Service\AuthService;
+
 /**
  * 测试已授权
  */
 class TestAuthController extends AuthorizedController {
 
+    /**
+     * 指定检测 web 平台
+     * @var string
+     */
+    protected $auth_paltform = AuthService::PLATFORM_WEB;
     /**
      * 已授权的可以看到下面文字
      */
